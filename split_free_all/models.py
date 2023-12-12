9455d6dd7f8ae38abaadb74b9c37a6cee4ce850b
Michael Lohier <lohiermichael@gmail.com>
2023-12-12 15:24:01 +0100

    models: create a first version
    There are 4 models:
    - user: any user of the app
    - event: it involves several users that may group to have expenses.
      You can think of "holidays", or a "outing"
    - expense: it involves several users, one of them being the payer. An
      expense is part of an event
    - userEventDebt: it's the debt balance of a user in an event as he has
      been involved in expenses
    
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

--- split_free_all/models.py
+++ split_free_all/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    users = models.ManyToManyField(User)
+
+
+class UserEventDebt(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    debt_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+
+
+class Expense(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    payer = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
