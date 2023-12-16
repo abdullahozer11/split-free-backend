97c495d62b48d8fa478ddf2fdc694f02b092b9ea
Michael Lohier <lohiermichael@gmail.com>
2023-12-16 17:11:52 +0100

    models: add __str__ methods
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

--- split_free_all/models.py
+++ split_free_all/models.py
@@ -6,17 +6,26 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f'User("{self.name}")'
+
 
 class Event(models.Model):
     title = models.CharField(max_length=255, default=None)
     description = models.TextField()
     users = models.ManyToManyField(User)
 
+    def __str__(self):
+        return f'Event("{self.title}")'
+
 
 class UserEventDebt(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     debt_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+
+    def __str__(self):
+        return f'User("{self.user.name}"): {self.debt_balance}'
 
 
 class Expense(models.Model):
@@ -26,3 +35,6 @@ class Expense(models.Model):
     payer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payer")
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     users = models.ManyToManyField(User, related_name="users")
+
+    def __str__(self):
+        return f'Expense("{self.title}") - Amount: {self.amount}'
