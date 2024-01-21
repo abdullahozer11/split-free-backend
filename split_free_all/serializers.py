4712a3a3e6fba6f900fb3b6e18c0aeb9496626dd
Abdullah OZER <abdullahozer11@hotmail.com>
2024-01-21 19:49:04 +0100

    React native will post data with member and group id's not the actual objects.
    Signed-off-by: Abdullah OZER <abdullahozer11@hotmail.com>

--- split_free_all/serializers.py
+++ split_free_all/serializers.py
@@ -17,11 +17,32 @@ class GroupSerializer(serializers.ModelSerializer):
         model = Group
         fields = "__all__"
 
+    def create(self, validated_data):
+        member_ids = validated_data.pop("members", None)
+        group = Group.objects.create(**validated_data)
+        group.members = []
+        for id in member_ids:
+            member = User.objects.get(pk=id)
+            group.members.append(member)
+        group.save()
+        return group
+
 
 class ExpenseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Expense
         fields = "__all__"
+
+    def create(self, validated_data):
+        group_id = validated_data.pop("group_id", None)
+        expense = Expense.objects.create(**validated_data)
+
+        if group_id:
+            group = Group.objects.get(pk=group_id)
+            expense.group = group
+            expense.save()
+
+        return expense
 
 
 class DebtSerializer(serializers.ModelSerializer):
