169c76a732af2bd257ca2c21a06bf9134505f334
Michael Lohier <lohiermichael@gmail.com>
2024-01-23 00:00:00 +0100

    aws: add command setup
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

---
+++ aws_ec2_setup.sh
@@ -0,0 +1,23 @@
+#! usr/bin/bash
+
+# Install Docker
+
+sudo apt update
+sudo apt upgrade
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install docker-ce
+sudo usermod -aG docker ${USER}
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker -v
+docker-compose -v
+
+# Install AWS CLI
+sudo apt install unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
