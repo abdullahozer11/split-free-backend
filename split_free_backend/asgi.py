2cf5789d05bea061e4fc0b2756d340c1d9b7441f
Abdullah OZER <abdullahozer11@hotmail.com>
2024-01-31 00:07:39 +0100

    settings is moved to settings folder. Staging and production requires separate settings files
    Signed-off-by: Abdullah OZER <abdullahozer11@hotmail.com>

--- split_free_backend/asgi.py
+++ split_free_backend/asgi.py
@@ -13,6 +13,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "split_free_backend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.base")
 
 application = get_asgi_application()
