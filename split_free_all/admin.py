2ceaf7b18c7e3bd9b9d38f063a9747290b17f866
Abdullah OZER <abdullahozer11@hotmail.com>
2024-02-01 20:17:59 +0100

    Custom admin model is created. Models are now editable in admin portal.
    Signed-off-by: Abdullah OZER <abdullahozer11@hotmail.com>

--- split_free_all/admin.py
+++ split_free_all/admin.py
@@ -3,3 +3,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from split_free_all.models import Balance, Expense, Group, Member, User
+
+admin.site.register(User)
+admin.site.register(Member)
+admin.site.register(Group)
+admin.site.register(Balance)
+admin.site.register(Expense)
--- split_free_all/models.py
+++ split_free_all/models.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023 SplitFree Org.
 
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 CURRENCY_CHOICES = [
@@ -12,11 +12,30 @@ CURRENCY_CHOICES = [
 ]
 
 
-class User(AbstractBaseUser):
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=32, null=True)
-    email = models.CharField(unique=True, max_length=128, default="example@hotmail.com")
+    email = models.EmailField(
+        unique=True, max_length=128, default="example@hotmail.com"
+    )
     password = models.CharField(max_length=32, null=True)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
     objects = UserManager()
