845dfe7bdcbc5cfb90eca2cc4643263307819a98
Abdullah OZER <abdullahozer11@hotmail.com>
2024-02-03 20:51:55 +0100

    Mock data no longer useful due to user model attachment.
    Signed-off-by: Abdullah OZER <abdullahozer11@hotmail.com>

--- remove_mock_data.py
+++
@@ -1,9 +0,0 @@
-# python manage.py shell < remove_mock_data.py
-
-from split_free_all.models import Balance, Debt, Expense, Group, Member
-
-Expense.objects.all().delete()
-Group.objects.all().delete()
-Member.objects.all().delete()
-Balance.objects.all().delete()
-Debt.objects.all().delete()
