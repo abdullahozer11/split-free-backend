9e6e1c367a59979ec70ab23c70a72c381a277a72
Abdullah OZER <abdullahozer11@hotmail.com>
2024-02-19 23:03:40 +0100

    Activity model is added to admin page.
    Signed-off-by: Abdullah OZER <abdullahozer11@hotmail.com>

--- split_free_all/admin.py
+++ split_free_all/admin.py
@@ -3,10 +3,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from split_free_all.models import Balance, Expense, Group, Member, User
+from split_free_all.models import Activity, Balance, Expense, Group, Member, User
 
 admin.site.register(User)
 admin.site.register(Member)
 admin.site.register(Group)
 admin.site.register(Balance)
 admin.site.register(Expense)
+admin.site.register(Activity)
