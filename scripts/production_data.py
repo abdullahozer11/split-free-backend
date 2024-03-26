7515b78c181b474186d0caf32e69c0e73b07f193
Abdullah OZER <abdullah.ozer@6wind.com>
2024-03-26 11:48:12 +0100

    utils: remove unused utils
    Signed-off-by: Abdullah OZER <abdullahozer11@hotmail.com>

--- scripts/production_data.py
+++ scripts/production_data.py
@@ -1,12 +1,6 @@
+# Copyright (c) 2024 SplitFree Org.
+
 from django.core.management.utils import get_random_secret_key
-
-from split_free_backend.core.utils.cryptography import generate_key_pair
-
-
-def generate_account():
-    key_pair = generate_key_pair()
-    print(f"Signing Key: {key_pair.private}")
-    print(f"Account Number: {key_pair.public}")
 
 
 def generate_secret_key():
@@ -16,5 +10,4 @@ def generate_secret_key():
 
 if __name__ == "__main__":
     print()
-    generate_account()
     generate_secret_key()
